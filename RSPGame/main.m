b14b5de7059149152226ba305fc4ecc07bfb23cb
Mostafa Saleh <mostafa.saleh@bugzy.net>
2018-06-30 23:56:10 +0200

    Initial Commit

---
+++ RSPGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RSPGame
+//
+//  Created by Mostafa Saleh on 6/30/18.
+//  Copyright © 2018 Mostafa Saleh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
